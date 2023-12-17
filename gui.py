fe69bce4bff7446f3191b98159a8b4ceff2bfbbd
CherryTreeMC <cherrytreemc@gmail.com>
2023-12-17 15:12:44 -0800

    Fixed font issues and GUI

--- gui.py
+++ gui.py
@@ -1,45 +1,53 @@
 import tkinter as tk
 from tkinter import colorchooser, simpledialog, font
-from tkinter.font import Font
-import customtkinter
-
+from tkinter.ttk import Combobox
 
 class GUI:
     def __init__(self):
-        customtkinter.set_appearance_mode("light")
-        self.root = customtkinter.CTk()
+        self.root = tk.Tk()
         self.root.title("Text Overlay")
         self.root.geometry("400x100+100+100")
         self.root.attributes("-topmost", True)
         self.root.overrideredirect(True)
         self.root.attributes('-alpha', 0.7)
-
-        self.text_label = customtkinter.CTkLabel(self.root, text="Your text will appear here", bg_color="white")
+        self.overrideredirect_enabled = True
+        
+        self.default_font = ("Helvetica", 18)
+        self.default_font_color = "black"
+        self.text_label = tk.Label(self.root, 
+                                   text="Your text will appear here", 
+                                   fg = self.default_font_color,
+                                   bg ="white")
         self.text_label.pack(expand=True, fill="both")
-        self.text_label.configure(font=("Helvetica", 16))  # Set font here
+        self.text_label.configure(font=self.default_font)
 
         # Settings icon
-        self.settings_icon = customtkinter.CTkLabel(self.root, text="⚙", font=("Helvetica", 14), fg_color="black")
+        self.settings_icon = tk.Label(self.root, text="⚙", 
+                                      font=self.default_font, 
+                                      fg=self.default_font_color, 
+                                      cursor="hand2")
         self.settings_icon.pack(anchor="ne")
         self.settings_icon.bind("<Button-1>", self.open_settings)
 
-        # Initialize variables for dragging and resizing
+        # Initialize variables for dragging
         self.start_x = 0
         self.start_y = 0
         self.drag_enabled = False
 
-        # Bind the events for dragging the window
+        # Bind events for dragging
         self.root.bind("<ButtonPress-1>", self.on_drag_start)
         self.root.bind("<B1-Motion>", self.on_drag_motion)
 
         # Menu for settings
-        self.settings_menu = customtkinter.CTkOptionMenu(self.root)
+        self.settings_menu = tk.Menu(self.root, tearoff=0)
         self.settings_menu.add_command(label="Toggle Dragging", command=self.toggle_dragging)
         self.settings_menu.add_command(label="Toggle Resizing", command=self.toggle_resizing)
         self.settings_menu.add_command(label="Change Background Color", command=self.change_color)
         self.settings_menu.add_command(label="Change Translucency", command=self.change_translucency)
         self.settings_menu.add_command(label="Change Font", command=self.change_font)
         self.settings_menu.add_command(label="Change Font Size", command=self.change_font_size)
+        self.settings_menu.add_command(label="Change Font Color", command=self.change_font_color)
+        self.settings_menu.add_command(label="Exit", command=self.exit_app)
 
     def open_settings(self, event):
         self.settings_menu.tk_popup(event.x_root, event.y_root)
@@ -48,16 +56,14 @@ class GUI:
         self.drag_enabled = not self.drag_enabled
 
     def toggle_resizing(self):
-        if self.root.overrideredirect():
-            self.root.overrideredirect(False)
-        else:
-            self.root.overrideredirect(True)
+        self.overrideredirect_enabled = not self.overrideredirect_enabled
+        self.root.overrideredirect(self.overrideredirect_enabled)
 
     def change_color(self):
         color = colorchooser.askcolor(title="Choose background color")[1]
         if color:
             self.root.config(bg=color)
-            self.text_label.config(bg_color=color)
+            self.text_label.config(bg=color)
 
     def change_translucency(self):
         alpha = simpledialog.askfloat("Translucency", "Enter value (0.1 - 1.0):", minvalue=0.1, maxvalue=1.0)
@@ -65,30 +71,38 @@ class GUI:
             self.root.attributes('-alpha', alpha)
 
     def change_font(self):
-        font_window = customtkinter.CTkToplevel(self.root)
+        font_window = tk.Toplevel(self.root)
         font_window.title("Select Font")
         font_window.geometry("200x150")
+        font_window.transient(self.root)  # Make the window modal
 
-        font_combobox = customtkinter.CTkComboBox(font_window, values=sorted(font.families()), width=180, height=25)
+        font_combobox = Combobox(font_window, values=sorted(font.families()), width=20)
         font_combobox.pack(pady=10)
 
         def font_selected(event):
             selected_font = font_combobox.get()
             if selected_font:
-                current_font_properties = font.nametofont(self.text_label.cget("font")).actual()
-                new_font = (selected_font, current_font_properties['size'])
-                self.text_label.config(text_font=new_font)
+                self.default_font = (selected_font, self.default_font[1])
+                self.text_label.configure(font=self.default_font, fg=self.default_font_color)
+                self.settings_icon.configure(font=self.default_font, fg=self.default_font_color)
                 font_window.destroy()
 
         font_combobox.bind("<<ComboboxSelected>>", font_selected)
 
     def change_font_size(self):
-        font_size = simpledialog.askinteger("Font Size", "Enter font size:", initialvalue=font.nametofont(self.text_label.cget("font")).actual()['size'])
-        
+        font_size = simpledialog.askinteger("Font Size", "Enter font size:", initialvalue=self.default_font[1])
+
         if font_size:
-            current_font = font.nametofont(self.text_label.cget("font"))
-            new_font = (current_font.actual()['family'], font_size)
-            self.text_label.config(text_font=new_font)
+            self.default_font = (self.default_font[0], font_size)
+            self.text_label.configure(font=self.default_font, fg=self.default_font_color)
+            self.settings_icon.configure(font=self.default_font, fg=self.default_font_color)
+            
+    def change_font_color(self):
+        color = colorchooser.askcolor(title="Choose font color")[1]
+        if color:
+            self.default_font_color = color
+            self.text_label.configure(font=self.default_font, fg=self.default_font_color)
+            self.settings_icon.configure(font=self.default_font, fg=self.default_font_color)
 
     def on_drag_start(self, event):
         if self.drag_enabled:
@@ -101,12 +115,12 @@ class GUI:
             y = self.root.winfo_y() + (event.y - self.start_y)
             self.root.geometry(f"+{x}+{y}")
 
-    def update_text(self, new_text):
-        self.text_label.config(text=new_text)
-        self.root.update_idletasks()
-
+    def exit_app(self):
+        self.root.destroy() # Destroy the root window   
+        
     def run(self):
-        self.root.mainloop()
+        self.root.mainloop() # Run the main loop 
+        
 
 if __name__ == "__main__":
     gui = GUI()
--- speech-recognition.py
+++ speech-recognition.py
@@ -4,45 +4,44 @@ import sys
 from threading import Thread
 from google.cloud import speech
 import pyaudio
-from gui_test import GUI
+import sounddevice as sd
 
 # Audio recording parameters
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
 
+
 class MicrophoneStream:
-    """Opens a recording stream as a generator yielding the audio chunks."""
-    def __init__(self, rate=RATE, chunk=CHUNK):
+    """Opens a loopback recording stream as a generator yielding the audio chunks."""
+    def __init__(self, rate=RATE, chunk=CHUNK, device=4):  # Adjust the device index as needed
         self._rate = rate
         self._chunk = chunk
+        self._device = device
         self._buff = queue.Queue()
         self.closed = True
 
     def __enter__(self):
-        self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            channels=1,  # API currently only supports 1-channel (mono) audio
-            rate=self._rate,
-            input=True,
-            frames_per_buffer=self._chunk,
-            stream_callback=self._fill_buffer,
-            input_device_index=4  # Adjust as needed
+        # Define the callback function for the SoundDevice stream
+        def callback(indata, frames, time, status):
+            # Convert the numpy array (indata) to bytes and put it in the buffer
+            self._buff.put(indata.tobytes())
+
+        # Open the SoundDevice stream for loopback
+        self._audio_stream = sd.InputStream(
+            device=8, 
+            channels=1, 
+            samplerate=self._rate,
+            callback=callback
         )
+        self._audio_stream.start()
         self.closed = False
         return self
 
     def __exit__(self, type, value, traceback):
-        self._audio_stream.stop_stream()
+        self._audio_stream.stop()
         self._audio_stream.close()
         self.closed = True
         self._buff.put(None)
-        self._audio_interface.terminate()
-
-    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        """Continuously collect data from the audio stream, into the buffer."""
-        self._buff.put(in_data)
-        return None, pyaudio.paContinue
 
     def generator(self):
         """Generates audio chunks from the stream."""
@@ -63,7 +62,7 @@ class MicrophoneStream:
 
             yield b"".join(data)
 
-def listen_print_loop(responses, gui):
+def listen_print_loop(responses):
     """Iterates through server responses and prints the transcription."""
     num_chars_printed = 0
     for response in responses:
@@ -77,14 +76,16 @@ def listen_print_loop(responses, gui):
         transcript = result.alternatives[0].transcript
 
         if result.is_final:
-            gui.update_text(transcript.lstrip() + '\r')
+            # gui.update_text(transcript.lstrip() + '\r')
+            print(transcript.lstrip() + '\r')
             num_chars_printed = 0
 
             if re.search(r"\b(exit|quit)\b", transcript, re.I):
                 print("Exiting..")
                 break
         else:
-            gui.update_text(transcript.lstrip() + '\r')
+            # gui.update_text(transcript.lstrip() + '\r')
+            print(transcript.lstrip() + '\r')
             num_chars_printed = len(transcript)
 
 def main():
@@ -102,9 +103,9 @@ def main():
         config=config, interim_results=True
     )
 
-    gui = GUI()
-    gui_thread = Thread(target=gui.run)
-    gui_thread.start()
+    # gui = GUI()
+    # gui_thread = Thread(target=gui.run)
+    # gui_thread.start()
 
     with MicrophoneStream(RATE, CHUNK) as stream:
         audio_generator = stream.generator()
@@ -112,7 +113,7 @@ def main():
                     for content in audio_generator)
 
         responses = client.streaming_recognize(streaming_config, requests)
-        listen_print_loop(responses, gui)
+        listen_print_loop(responses)
 
 if __name__ == "__main__":
     main()
